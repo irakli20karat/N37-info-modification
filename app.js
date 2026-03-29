cb31d0b03b3855ee78c9a49b3ca041aa542d0ca4
03Heraklion <irakli20karat>
2026-03-29 16:54:49 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const sanitizeName = (req, res, next) => {
+    req.body.name = req.body?.name.trim().toUpperCase();
+    next();
+}
+
+app.use(express.json());
+app.use(sanitizeName);
+
+app.post('/create-user', (req, res) => {
+    res.send(`მომხმარებელი ${req.body.name} შეიქმნა`);
+});
+
+app.listen(port, () => {
+    console.log("Server is running on port", port);
+});
